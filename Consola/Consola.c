52ccff3dc62ec27c0f0f4de048bfa53ae31cb858
kevin-vargas <kevinn.vargass@gmail.com>
2018-04-20 14:44:32 -0300

    Se agregaron logeos a consola planificador y coordinador, falta esi instancias.

--- Consola/Consola.c
+++ Consola/Consola.c
@@ -5,6 +5,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <commons/string.h>
+#include <commons/log.h>
 
 int cantidadDeCentinelas(char** centinelas){
 	int i = 0;
@@ -15,6 +16,7 @@ int cantidadDeCentinelas(char** centinelas){
 }
 
 int main() {
+	t_log *logger=log_create("/home/utnso/git/tp-2018-1c-UAL-masters/Logs/Planificador.log","Consola",0, LOG_LEVEL_INFO);
   char* linea;
   while(1) {
     linea = readline(">");
@@ -30,36 +32,45 @@ int main() {
     	case 1:
     		if(!strcmp(centinelas[0],"continuar")){
     			printf("Usted ingreso continuar\n");
+    			log_info(logger, "Se ingreso comando continuar");
     		}
     		else if(!strcmp(centinelas[0],"pausar")){
         		printf("Usted ingreso pausar\n");
+        		log_info(logger, "Se ingreso comando pausar");
     		}
     	    else if(!strcmp(centinelas[0],"deadlock")){
     	    		printf("Usted ingreso deadlock\n");
+    	    		log_info(logger, "Se ingreso comando deadlock");
     	    }
     	    else{
     	    	printf("No se reconocio el comando %s\n", centinelas[0]);
+    	    	log_error(logger, "No se reconocio el comando");
     	    }
     		break;
     	case 2:
 	   		if(!strcmp(centinelas[0],"desbloquear")){
 	    		printf("Usted ingreso desbloquear\n");
 	    		printf("con la clave: %s\n", centinelas[1]);
+	    		log_info(logger, "Se ingreso comando desbloquear");
 	    	}
 	   		else if(!strcmp(centinelas[0],"listar")){
 	        	printf("Usted ingreso listar\n");
 	        	printf("con el recurso: %s\n",centinelas[1]);
+	        	log_info(logger, "Se ingreso comando listar");
 	        }
 	   		else if(!strcmp(centinelas[0],"kill")){
 	        	printf("Usted ingreso kill\n");
 	        	printf("con el id: %s\n",centinelas[0]);
+	        	log_info(logger, "Se ingreso comando Kill");
 	        	}
 	   	    else if(!strcmp(centinelas[0],"status")){
 	   	    	printf("Usted ingreso status\n");
 	   	    	printf("con la clave %s\n",centinelas[1]);
+	   	    	log_info(logger, "Se ingreso comando status");
 	   	    }
     	    else{
     	    	printf("No se reconocio el comando %s\n", centinelas[0]);
+    	    	log_error(logger, "No se reconocio el comando");
     	    }
     		break;
     	case 3:
@@ -67,14 +78,18 @@ int main() {
 		    	printf("Usted ingreso bloquear\n");
 		    	printf("con la clave: %s\n", centinelas[1]);
 		    	printf("con el id: %s\n",centinelas[2]);
+		    	log_info(logger, "Se ingreso comando bloquear");
 			}
 			else{
     	    	printf("No se reconocio el comando %s\n", centinelas[0]);
+    	    	log_error(logger, "No se reconocio el comando");
 			}
     		break;
     	default:
     		printf("Usted ingreso una cantidad de argumentos invalida\n");
+    		log_error(logger, "Cantidad de argumentos invalida");
     }
+    log_destroy(logger);
     free(centinelas);
     free(linea);
   }
--- Coordinador/Coordinador.c
+++ Coordinador/Coordinador.c
@@ -13,6 +13,7 @@
 #include <arpa/inet.h>
 #include "FuncionesConexiones.h"
 #include <commons/string.h>
+#include <commons/log.h>
 
 typedef struct{
 	int a;
@@ -33,11 +34,24 @@ Paquete deserializacion(char* texto){
 	return pack;
 }
 void crearSelect(int soyCoordinador,char *pathYoServidor,char *pathYoCliente){// en el caso del coordinador el pathYoCliente lo pasa como NULL
+	 char* path;
+	 int listener;
+	 if(soyCoordinador)
+		path="/home/utnso/git/tp-2018-1c-UAL-masters/Logs/Coordinador.log";
+	 else
+		path="/home/utnso/git/tp-2018-1c-UAL-masters/Logs/Planificador.log";
+	 t_log *logger=log_create(path,"crearSelect",1, LOG_LEVEL_INFO);
 	 fd_set master;   // conjunto maestro de descriptores de fichero
 	 fd_set read_fds; // conjunto temporal de descriptores de fichero para select()
 	 struct sockaddr_in their_addr; // datos cliente
 	 int fdmax;        // el descriptor mas grande
-	 int listener=crearConexionServidor(pathYoServidor);
+	 if((listener=crearConexionServidor(pathYoServidor))==-1){
+		 log_error(logger, "No se pudo crear el socket servidor");
+		 log_destroy(logger);
+		 exit(-1);
+	 }
+	 else
+		 log_info(logger, "Se creao el socket de Servidor");
      int nuevoCliente;        // descriptor de socket de nueva conexión aceptada
      char *buf=malloc(1024);    // buffer para datos del cliente
      int nbytes;
@@ -49,20 +63,39 @@ void crearSelect(int soyCoordinador,char *pathYoServidor,char *pathYoCliente){//
      FD_ZERO(&read_fds);	// borra los conjuntos maestro
 
      if(soyCoordinador==0){
-    	casoDiscriminador=crearConexionCliente(pathYoCliente);
+    	if((casoDiscriminador=crearConexionCliente(pathYoCliente))==-1){
+    		 log_error(logger, "No se pudo crear socket de cliente");
+    		 log_destroy(logger);
+    		 exit(-1);
+    	}
+    	else
+    		log_info(logger, "Se creo el socket de cliente");
      }
-     if (listen(listener, 10) == -1) {
-         perror("listen");
-         exit(1);
+     if (listen(listener, 10) == -1){
+    	 log_error(logger, "No se pudo escuchar");
+		 log_destroy(logger);
+		 exit(-1);
      }
-     printf("Escuchando\n");
+     else
+    	 log_info(logger, "Se esta escuchando");
      if(soyCoordinador==1){
     	 addrlen = sizeof(their_addr);
-    	 casoDiscriminador = accept(listener, (struct sockaddr *)&their_addr,&addrlen);
+    	 if((casoDiscriminador = accept(listener, (struct sockaddr *)&their_addr,&addrlen))==-1){
+    		 log_error(logger, "No se aceptar la conexion");
+    		 log_destroy(logger);
+    		 exit(-1);
+    	 }
+    	 else
+    		 log_info(logger, "Se acepto la conexion");
     	 printf("Nuevo cliente, se conecto el Planificador\n");
-    	  fflush(stdout);
-    	 send(casoDiscriminador,"Hola capo soy el Coordinador\n",1024,0);
+    	 fflush(stdout);
+    	 if(send(casoDiscriminador,"Hola capo soy el Coordinador\n",1024,0)==-1){
+    		 log_error(logger, "No se pudo enviar el mensaje");
+    	 }
+    	 else
+    		 log_info(logger, "Mensaje enviado correctamente");
      }
+
      FD_SET(listener, &master);
      FD_SET(casoDiscriminador, &master);
      if(casoDiscriminador>listener)
@@ -72,30 +105,35 @@ void crearSelect(int soyCoordinador,char *pathYoServidor,char *pathYoCliente){//
      for(;;) {
                  read_fds = master; // cópialo
                  if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
-                     perror("select");
-                     exit(1);
+                	 log_error(logger, "Error Al seleccionar");
+                	 exit(-1);
                  }
+                 else
+                	 log_info(logger, "Se selecciono correctamente");
                  // explorar conexiones existentes en busca de datos que leer
                  for(i = 0; i <= fdmax; i++) {
                      if (FD_ISSET(i, &read_fds)) { // ¡¡tenemos datos!!
                          if (i == listener) {
+                        	 log_info(logger, "Conexion entrante del listener");
                              // gestionar nuevas conexiones
                              addrlen = sizeof(their_addr);
                              if ((nuevoCliente = accept(listener, (struct sockaddr *)&their_addr,
                                                                       &addrlen)) == -1) {
-                                 perror("accept");
+                            	 log_error(logger, "Al Aceptar al nuevo cliente");
                              } else {
                                  FD_SET(nuevoCliente, &master); // añadir al conjunto maestro
                                  if (nuevoCliente > fdmax) {    // actualizar el máximo
                                      fdmax = nuevoCliente;
                                  }
                                  printf("Nuevo cliente\n");
+                                 log_info(logger, "Ingreso un nuevo cliente");
                                  fflush(stdout);
                                  send(nuevoCliente,"Hola capo soy tu Servidor Select\n",1024,0);
                              }
 
                          }
                          else if(i==casoDiscriminador){//aca trato al coordinador//Caso discriminador
+                        	log_info(logger, "Conexion entrante del discriminador");
                          	recv(casoDiscriminador,buf,1024,0);//funcion relacionada
                          	printf("%s\n",buf);
                          	fflush(stdout);
@@ -106,13 +144,16 @@ void crearSelect(int soyCoordinador,char *pathYoServidor,char *pathYoCliente){//
                                  // error o conexión cerrada por el cliente
                                  if (nbytes == 0) {
                                      // conexión cerrada
+                                	 log_info(logger, "El cliente se fue");
                                      printf("selectserver: socket %d hung up\n", i);
                                  } else {
+                                	 log_info(logger, "Problema de conexion con el cliente");
                                      perror("recv");
                                  }
                                  close(i); // cierra socket
                                  FD_CLR(i, &master); // eliminar del conjunto maestro
                              } else {
+                            	log_info(logger, "Conexion entrante del cliente");
                                printf("%s\n",buf);
                                fflush(stdout);
                                send(i,"Dale Esi",1024,0);
