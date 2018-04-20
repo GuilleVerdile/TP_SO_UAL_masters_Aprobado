6d0d9c7d3cd66c88e6eb94a036acf297b9f4e073
kevin-vargas <kevinn.vargass@gmail.com>
2018-04-19 23:43:34 -0300

    Avanzamos con el protocolo la funcion transformarTamagnoKey no esta funcionando

--- ESI/ESI.c
+++ ESI/ESI.c
@@ -13,10 +13,42 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include "FuncionesConexiones.h"
+#include <commons/string.h>
+const int SET=0;
+const int GET=1;
+const int STORE=2;
+
+typedef struct{
+	int a;
+	char key[40];
+	char *value;
+}Paquete;
+
+void transformarTamagnoKey(char* key,char *resultado){
+	int tam=string_length(key);
+	if(tam<10){
+		resultado="0";
+		strcat(resultado,string_itoa(tam));
+	}
+	else
+		resultado=string_itoa(tam);
+}
+char *serealizarPaquete(Paquete pack){
+	char* serealizado =string_itoa(pack.a);
+	if(!pack.a){
+	string_append(&serealizado, &(pack.key));
+	}
+	string_append(&serealizado, &(pack.key));
+	if(!pack.a){
+	string_append(&serealizado, pack.value);
+	}
+
+	return serealizado;
+}
 
 
-int esi(char* pathCoordinador,char*pathPlanificador){
-	char* buffer = malloc(1024);
+int esi(char* pathCoordinador,char*pathPlanificador,Paquete pack){
+		char* buffer = malloc(1024);
 		int sockplanificador=crearConexionCliente(pathPlanificador);
 		int sockcoordinador=crearConexionCliente(pathCoordinador);
 	   printf("Se crearon sockets cliente!");
@@ -25,21 +57,22 @@ int esi(char* pathCoordinador,char*pathPlanificador){
 		   return 1;
 	   }
 	   printf("Se conecto a los 2 servidores\n");
-	   //me avisan todo bien
-	   recv(sockplanificador,buffer,1024,0);
-	   printf("%s",buffer);
-	   recv(sockcoordinador,buffer,1024,0);
-	   printf("%s",buffer);
-	   send(sockplanificador,"Hola soy el cliente :D",1024,0);
-	   send(sockcoordinador,"Hola soy el cliente :D",1024,0);
-	   free(buffer);
-	 return 0;
+
+	   return 0;
 }
 int main(){
-	 char *pathCoordinador="/home/utnso/git/tp-2018-1c-UAL-masters/Config/Coordinador.cfg";
-			 char *pathPlanificador="/home/utnso/git/tp-2018-1c-UAL-masters/Config/Planificador.cfg";
-			 esi(pathCoordinador,pathPlanificador);
-			 while(1){}
+	/*Paquete pack;
+	pack.a=SET;
+	strcpy (pack.key,"MILLAVE");
+	pack.value="MIVALOR";
+	char *pathCoordinador="/home/utnso/git/tp-2018-1c-UAL-masters/Config/Coordinador.cfg";
+	char *pathPlanificador="/home/utnso/git/tp-2018-1c-UAL-masters/Config/Planificador.cfg";
+	esi(pathCoordinador,pathPlanificador);
+	char *buff=serealizar(pack);
+	while(1){}*/
+	char *a=malloc(1024);
+	transformarTamagnoKey("HOLAHOLA",a);
+	printf("%s",a);
 	return 0;
 }
 
