65e33b21cc9f6a2cd44d3b637368f445591cf60f
kevin-vargas <kevinn.vargass@gmail.com>
2018-06-09 19:58:16 -0300

    Agregando colores

--- Planificador/Colores.c
+++ Planificador/Colores.c
@@ -11,3 +11,30 @@ char *colorear(color col,char *palabra){
 	return string_from_format("\x1b[1;3%dm%s%s",col,palabra,"\x1b[0m");
 }
 
+char *verde(char *palabra){
+	return colorear(Verde,palabra);
+}
+
+char *rojo(char *palabra){
+	return colorear(Rojo,palabra);
+}
+
+char *blanco(char *palabra){
+	return colorear(Blanco,palabra);
+}
+
+char *amarillo(char *palabra){
+	return colorear(Amarillo,palabra);
+}
+
+char *azul(char *palabra){
+	return colorear(Azul,palabra);
+}
+
+char *magenta(char *palabra){
+	return colorear(Magenta,palabra);
+}
+
+char *cian(char *palabra){
+	return colorear(Cian,palabra);
+}
--- Planificador/Colores.h
+++ Planificador/Colores.h
@@ -14,5 +14,18 @@ typedef enum {Negro,Rojo,Verde,Amarillo,Azul,Magenta,Cian,Blanco} color;
 
 char *colorear(color col,char *palabra);
 
+char *verde(char *palabra);
+
+char *rojo(char *palabra);
+
+char *blanco(char *palabra);
+
+char *amarillo(char *palabra);
+
+char *azul(char *palabra);
+
+char *magenta(char *palabra);
+
+char *cian(char *palabra);
 
 #endif /* COLORES_H_ */
